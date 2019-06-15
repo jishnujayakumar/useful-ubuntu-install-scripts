2f97d29184bb71421c1b3a6156a95c31f86c89fa
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2019-06-15 09:46:21 +0530

    ADD virtualenvwrapper install script

---
+++ install_virtualenvwrapper.sh
@@ -0,0 +1,18 @@
+# @source: https://medium.com/@aaditya.chhabra/virtualenv-with-virtualenvwrapper-on-ubuntu-34850ab9e765 &
+#          https://www.learnopencv.com/install-opencv-3-4-4-on-ubuntu-16-04/
+
+echo "Installing VirtualEnvWrapper"
+echo ""
+sudo apt -y install python3-dev python3-pip python3-venv
+sudo apt-get install python-pip
+sudo pip install virtualenv
+mkdir ~/.virtualenvs
+sudo pip install virtualenvwrapper
+. /usr/local/bin/virtualenvwrapper.sh
+source ~/.bashrc
+
+echo "Done"
+echo ""
+echo "Create a new virtualenv: mkvirtualenv <your-project-name>"
+echo "To exit your new virtualenv, use deactivate"
+echo "To load or switch between virtualenvs: workon <your-project-name>"
