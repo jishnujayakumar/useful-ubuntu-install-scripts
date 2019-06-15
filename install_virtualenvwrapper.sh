6551858cb1c445326f2597f65b79a794b0f48610
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2019-06-15 10:27:50 +0530

    update

--- install_virtualenvwrapper.sh
+++ install_virtualenvwrapper.sh
@@ -1,13 +1,11 @@
 # @source: https://medium.com/@aaditya.chhabra/virtualenv-with-virtualenvwrapper-on-ubuntu-34850ab9e765 &
 #          https://www.learnopencv.com/install-opencv-3-4-4-on-ubuntu-16-04/
 
-echo "Installing VirtualEnvWrapper"
 echo ""
-sudo apt -y install python3-dev python3-pip python3-venv
-sudo apt-get install python-pip
-sudo pip install virtualenv
-mkdir ~/.virtualenvs
-sudo pip install virtualenvwrapper
+echo "Installing VirtualEnvWrapper"
+sudo apt -y install python3-dev python3-pip python3-venv python-pip > /dev/null 2>&1
+sudo pip install -q virtualenv virtualenvwrapper
+mkdir -p ~/.virtualenvs
 . /usr/local/bin/virtualenvwrapper.sh
 export WORKON_HOME=~/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
@@ -15,6 +13,10 @@ source ~/.bashrc
 
 echo "Done"
 echo ""
-echo "Create a new virtualenv: mkvirtualenv <your-project-name>"
-echo "To exit your new virtualenv, use deactivate"
-echo "To load or switch between virtualenvs: workon <your-project-name>"
+echo "====================================================================="
+echo "* Create a new virtualenv: mkvirtualenv <your-project-name>         *"
+echo "====================================================================="
+echo "* To exit your new virtualenv, use deactivate                       *"
+echo "====================================================================="
+echo "* To load or switch between virtualenvs: workon <your-project-name> *"
+echo "====================================================================="
